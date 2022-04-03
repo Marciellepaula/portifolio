6b679dee6405af4acac350ee74b1b2b0b4563d09
Marciellepaula <63891364+Marciellepaula@users.noreply.github.com>
2022-04-03 04:06:04 -0300

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,22 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('on')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('on')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
